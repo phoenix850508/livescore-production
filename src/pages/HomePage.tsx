6b64365a26ee35ff716ba3fa5d77dec0b2c808cd
phoenix850508 <tom850508@gmail.com>
2023-07-21 01:18:31 +0800

    fix: automatically navigte from HomePage to MainPage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 export default function HomePage() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/main");
+  }, [navigate]);
   return (
     <div>
       <h3>homepage</h3>
