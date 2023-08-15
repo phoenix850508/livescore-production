94eeadec5430178c28754b15d7cacec0722c6182
phoenix850508 <tom850508@gmail.com>
2023-08-16 00:50:51 +0800

    fix: move package.json to the root directory

--- src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-import MainPage from "./MainPage";
-import HomePage from "./HomePage";
-import MatchInfoPage from "./MatchInfoPage";
-import LeagueInfoPage from "./LeagueInfoPage";
-import TeamInfoPage from "./TeamInfoPage";
-
-export { MainPage, HomePage, MatchInfoPage, LeagueInfoPage, TeamInfoPage };
